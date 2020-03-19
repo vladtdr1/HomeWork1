ee375fc3a5ed82f6fc7a1712df0d73b81c5630b5
vladtdr1 <vladtdr1@gmail.com>
2020-03-19 16:44:16 +0200

    did intellij stuff

--- lab1-master/src/loose/oose/fis/documents/Document.java
+++ lab1-master/src/loose/oose/fis/documents/Document.java
@@ -1,10 +1,10 @@
 package loose.oose.fis.documents;
 
 public abstract class Document {
-    protected String[] continut;
+    protected String[] conti;
 
     public Document(String[] continut) {
-        this.continut = continut;
+        this.conti = continut;
     }
 
     public abstract String[] analizeaza();
@@ -12,7 +12,7 @@ public abstract class Document {
     @Override
     public String toString() {
         String res = "";
-        for (String cuvant : continut) {
+        for (String cuvant : conti) {
             res += cuvant + " ";
         }
         return res;
